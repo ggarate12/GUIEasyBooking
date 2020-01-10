4ac235910bea6157fe91b0cffde9518bc415fae4
ggarate12 <gorka.garate@opendeusto.es>
2020-01-10 14:24:22 +0100

    Falta terminar los passwordField

--- GUIEasyBooking/src/Ventanas/VentanaRegistrar.java
+++ GUIEasyBooking/src/Ventanas/VentanaRegistrar.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JPasswordField;
 
 public class VentanaRegistrar extends JFrame{
 	
@@ -30,8 +31,8 @@ public class VentanaRegistrar extends JFrame{
 	private JTextField tfNombre;
 	private JTextField tfEmail;
 	private JTextField tfEdad;
-	private JTextField tfPassword;
-	private JTextField tfRepitePasssword;
+	private JPasswordField pfPassword;
+	private JPasswordField pfRepitePassword;
 	public VentanaRegistrar() {
 		
 		arrayAeropuertos.add(bcn);
@@ -64,16 +65,6 @@ public class VentanaRegistrar extends JFrame{
 		getContentPane().add(tfEdad);
 		tfEdad.setColumns(10);
 		
-		tfPassword = new JTextField();
-		tfPassword.setBounds(188, 100, 96, 20);
-		getContentPane().add(tfPassword);
-		tfPassword.setColumns(10);
-		
-		tfRepitePasssword = new JTextField();
-		tfRepitePasssword.setBounds(188, 125, 96, 20);
-		getContentPane().add(tfRepitePasssword);
-		tfRepitePasssword.setColumns(10);
-		
 		JLabel lblEdad = new JLabel("Edad");
 		lblEdad.setBounds(47, 77, 81, 14);
 		getContentPane().add(lblEdad);
@@ -102,17 +93,25 @@ public class VentanaRegistrar extends JFrame{
 		JButton btnRegistrar = new JButton("Registrar");
 		btnRegistrar.setBounds(195, 215, 89, 23);
 		getContentPane().add(btnRegistrar);
+		
+		pfPassword = new JPasswordField();
+		pfPassword.setBounds(188, 100, 96, 20);
+		getContentPane().add(pfPassword);
+		
+		pfRepitePassword = new JPasswordField();
+		pfRepitePassword.setBounds(188, 125, 96, 20);
+		getContentPane().add(pfRepitePassword);
 		btnRegistrar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(controller.comprobarContrasenyas(password, passwordRepetido) == true) {
-					controller.crearUsuario(tfNombre.getText(), tfEmail.getText(), Integer.parseInt(tfEdad.getText()), cbAeropuertos.getSelectedItem().toString(), tfPassword.getText());
-					tfNombre.setText("");
-					tfEmail.setText("");
-					tfEdad.setText("");
-					tfPassword.setText("");
-					tfRepitePasssword.setText("");
-				}
+//				if(controller.comprobarContrasenyas(pfPassword.getPassword(), pfRepitePassword.getText()) == true) {
+//					controller.crearUsuario(tfNombre.getText(), tfEmail.getText(), Integer.parseInt(tfEdad.getText()), cbAeropuertos.getSelectedItem().toString(), pfPassword.getText());
+//					tfNombre.setText("");
+//					tfEmail.setText("");
+//					tfEdad.setText("");
+//					pfPassword.setText("");
+//					pfRepitePassword.setText("");
+//				}
 			}
 		});
 	}
